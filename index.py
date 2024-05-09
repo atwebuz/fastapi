61b42a25a00c5e5176fa2376d743ad104aaa5d26
atwebuz <abdusattorov2004@mail.ru>
2024-05-10 00:57:11 +0500

    middlewares was added

--- index.py
+++ index.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from routes.user import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origns=['*'],
+    allow_credentials=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+
+)
 app.include_router(user)
--- routes/user.py
+++ routes/user.py
@@ -2,20 +2,26 @@ from fastapi import APIRouter
 from config.db import conn
 from models.user import users
 from schemas.user import User
+
 user = APIRouter()
 
 @user.get('/')
-def fetch_user():
+async def fetch_users():
+    # Return all users
     return conn.execute(users.select()).fetchall()
 
 @user.post('/')
-def post_user():
+async def create_user(user: User):
+    # Insert a new user
     return conn.execute(users.insert().values(name=user.name, email=user.email, password=user.password))
 
 @user.put('/{id}')
-def update_user(id: int, user: User):
+async def update_user(id: int, user: User):
+    # Update an existing user by ID
     return conn.execute(users.update().values(name=user.name, email=user.email, password=user.password).where(users.c.id == id))
 
 @user.delete('/{id}')
-def delete_user(id: int):
-    return conn.execute(users.delete().where(users.c.id == id)).fetchall()
+async def delete_user(id: int):
+    # Delete a user by ID
+    return conn.execute(users.delete().where(users.c.id == id))
+
