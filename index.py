f85641f782efa0f02982c0aa82089e67f73cc38f
atwebuz <abdusattorov2004@mail.ru>
2024-05-10 17:53:28 +0500

    fixed

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origns=['*'],
+    allow_origins=['*'],
     allow_credentials=['*'],
     allow_methods=['*'],
     allow_headers=['*'],
